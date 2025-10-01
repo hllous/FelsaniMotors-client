f838768f44239f92b92a6a8fd7ef136dfbbeb048
nicof <nico.llousas@gmail.com>
2025-10-01 19:27:14 -0300

    Mas implementacion de taiwlind, con paletas custom

---
+++ src/views/TailwindMockTest.jsx
@@ -0,0 +1,19 @@
+
+
+const TailwindMockTest = () => {
+  return (
+    <body class="min-w-screen min-h-screen mx-auto border border-black">
+        <header class="text-white text">
+            header
+        </header>
+        <main class="text-white text">
+            main
+        </main>
+        <footer class="text-white text">
+            footer
+        </footer>
+    </body>
+  );
+};
+
+export default TailwindMockTest;
