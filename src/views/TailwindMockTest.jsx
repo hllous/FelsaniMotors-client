24481db4272d3924287376f9e8bc641e2bdc77c5
nicof <nico.llousas@gmail.com>
2025-10-02 17:56:27 -0300

    Creacion de los archivos de componentes globales. Falta implementacion

--- src/views/TailwindMockTest.jsx
+++ src/views/TailwindMockTest.jsx
@@ -2,11 +2,11 @@
 
 const TailwindMockTest = () => {
   return (
-    <body class="min-w-screen min-h-screen mx-auto border border-black">
+    <body class="min-w-screen min-h-screen mx-auto border border-black bg-paleta1-blue">
         <header class="text-white text">
             header
         </header>
-        <main class="text-white text">
+        <main class="text-paleta1-cream-light">
             main
         </main>
         <footer class="text-white text">
